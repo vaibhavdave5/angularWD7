37e0e682de3c5d443776101cbfd5419e814ef837
Vaibhav Shekhar Dave <vaibhavdave5@ccs.neu.edu>
2019-03-19 19:52:34 -0400

    Service in good shape

--- src/app/course-grid-component/course-grid-component.component.ts
+++ src/app/course-grid-component/course-grid-component.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {CourseService} from "../services/course.service";
 
 @Component({
   selector: 'app-course-grid-component',
@@ -7,12 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseGridComponentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private courseService: CourseService) { }
 
   ngOnInit() {
-    // this.service
-    //   .findAllCourses()
-    //   .then(courses => console.log(courses))
+    this.courseService.findAllCourses().
+    then(course => console.log(course))
+
   }
 
 
--- src/app/services/course.service.ts
+++ src/app/services/course.service.ts
@@ -6,4 +6,14 @@ import { Injectable } from '@angular/core';
 export class CourseService {
 
   constructor() { }
+
+  findAllCourses = () => {
+    return fetch('http://localhost:8080/api/courses', {
+      credentials: "include",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: 'GET'
+    }).then(response => response.json());
+  }
 }
